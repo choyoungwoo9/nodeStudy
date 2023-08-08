79e35fdb1e77e8e93c051d82afbb2e6e98c5bf0a
Youngwoo Cho <youngwch@c5r5s6.42seoul.kr>
2023-08-08 20:13:46 +0900

    응답 전송

--- section3/app.js
+++ section3/app.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 
 const server = http.createServer(function(req, res){
-	console.log(req);
+	console.log(req.url, req.method, req.headers);
 	// console.log(res);
+	//브라우저에게 HTML코드라는것을 알려줌
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>My FirstPage</title></head>');
+	res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+	res.write('</html>');
+	res.end();
+	//오류 생김
+	// res.write("abc");
 });
 
 server.listen(3000);
