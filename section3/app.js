25778f3b6bce5eed9ebf967c49621584d1d27f63
Youngwoo Cho <youngwch@c5r5s6.42seoul.kr>
2023-08-10 16:13:15 +0900

    리다이렉트 후 데이터 쓰기 완료

--- section3/app.js
+++ section3/app.js
@@ -1,8 +1,32 @@
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer(function(req, res){
-	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	const method = req.method;
+	if(url == '/'){
+		res.write('<html>');
+		res.write('<head><title>Enter Message</title></head>');
+		res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+		res.write('</html>');
+		return res.end();
+	}
+	if(url === '/message' && method === 'POST')
+	{
+		const body = [];
+		req.on('data', (chunk)=>{
+			console.log(chunk);
+			body.push(chunk);
+		});
+		req.on('end', ()=>{
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+			fs.writeFileSync('message.txt', message);
+		});
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
+		return res.end();
+	}
 	// console.log(res);
 	//브라우저에게 HTML코드라는것을 알려줌
 	res.setHeader('Content-Type', 'text/html');
