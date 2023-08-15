2dd89c4b8d23e5e0a9b8be548a30078d0a50c3f4
Youngwoo Cho <youngwch@c5r6s6.42seoul.kr>
2023-08-15 13:41:17 +0900

    style 추가

--- section5/app.js
+++ section5/app.js
@@ -1,6 +1,7 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 
@@ -8,11 +9,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-	res.status(404).sendFile(path.join(__dirname, './views', '404.html'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
